ac49f19fa7da96e92de0ac11814f9a8e3f5c1dc0
itsGhazle <102896945+itsGhazle@users.noreply.github.com>
2023-02-25 00:08:26 +0330

    issue fixed!!!

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 let originalLink = [];
 let shortLink = [];
-
+// localStorage.clear()
 $(document).ready(function () {
   getToStorage();
-  getTable();
+  getTable(getToStorage());
 });
 
 function timeOut() {
@@ -11,16 +11,23 @@ function timeOut() {
     $("p").removeClass("error");
   }, 3000);
 }
-function getTable() {
-  if (!originalLink.length == 0) {
+function getToStorage() {
+  if (!(localStorage.length == 0)) {
+    originalLink = JSON.parse(localStorage["original"]);
+    shortLink = JSON.parse(localStorage["short"]);
+  }
+  return [originalLink, shortLink];
+}
+function getTable(data) {
+  if (!data[0].length == 0) {
     $(".addLinks").empty();
-    for (let i = 0; i < originalLink.length; i++) {
+    for (let i = 0; i < data[0].length; i++) {
       $(".addLinks").append(`<tr>
       <td class="original_links" colspan=2>
-        <a href="${originalLink[i]} target="_blank"">${originalLink[i]}</a>
+        <a href="${data[0][i]} target="_blank"">${data[0][i]}</a>
       </td>
       <td class="short redirect" colspan=2>
-        <a href="${originalLink[i]}" target="_blank">${shortLink[i]}</a>
+        <a href="${data[0][i]}" target="_blank">${data[1][i]}</a>
       </td>
     </tr>`);
     }
@@ -33,67 +40,45 @@ function getTable() {
   }
 }
 
-
-function getToStorage() {
-  if (!(localStorage.length == 0)) {
-    originalLink = JSON.parse(localStorage["original"]);
-    shortLink = JSON.parse(localStorage["short"]);
-  }
-  return [originalLink, shortLink];
-}
-function addLinksToTable(data) {
-  $(".addLinks").append(`<tr>
-      <td class="original_links" colspan=2>
-        <a href="${data[0]} target="_blank"">${data[0]}</a>
-      </td>
-      <td class="short redirect" colspan=2>
-        <a href="${data[0]}" target="_blank">${data[1]}</a>
-      </td>
-    </tr>`);
-}
 function setToStorage(data) {
   let storage = getToStorage();
   storage[0].push(data[0]);
   localStorage.setItem("original", JSON.stringify(storage[0]));
   localStorage.setItem("short", JSON.stringify(data[1]));
 }
-
 function in_array(array, data) {
   for (var i = 0; i < array.length; i++) if (array[i] == data) return true;
   return false;
 }
 
 function get_rand() {
-  let Txt = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`;
   let shortUrl = "";
+  let Txt = `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`;
+
   for (let i = 0; i < 6; i++) {
     shortUrl += Txt.charAt(Math.floor(Math.random() * Txt.length));
   }
 
   if (!in_array(shortLink, shortUrl)) {
     shortLink.push(shortUrl);
-    
-    return shortUrl;
   } else {
-    shortUrl=""
+    console.log("duplicate");
+    shortUrl = "";
     Txt = Txt + String(new Date().getSeconds());
     for (let i = 0; i < 6; i++) {
       shortUrl += Txt.charAt(Math.floor(Math.random() * Txt.length));
     }
-    return shortUrl;
+    shortLink.push(shortUrl);
   }
 }
 function createRanString() {
   let url = $("#UrlInp").val();
   get_rand();
   setToStorage([url, shortLink]);
-  addLinksToTable([url, get_rand()]);
-  getTable();
+  getTable(getToStorage());
 }
 
-function checkUrl(e) {
-  e.preventDefault;
-  this.disabled = true;
+function checkUrl() {
   let url = $("#UrlInp").val();
   if (url === "") {
     $("p").html("Please enter a url!").addClass("error");
@@ -110,7 +95,13 @@ function checkUrl(e) {
     }
   }
   $("#UrlInp").val("");
-  this.disabled = false;
+  $("#UrlInp").focus();
+ 
 }
 
-$(".submit-btn").on("click", checkUrl);
+$("#shortenUrl").on("click", function (e) {
+  checkUrl();
+});
+$("#UrlInp").on("keyup", function (e) {
+  if (e.key === "Enter") checkUrl();
+});
